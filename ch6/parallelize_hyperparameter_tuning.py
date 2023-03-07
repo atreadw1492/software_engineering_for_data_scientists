d5a0167c06f3d4473f61fe1b38c1cf811b273d76
Andrew <atreadw114@gmail.com>
2023-03-06 20:40:43 -0800

    update hyperparameter tuning script

--- ch6/parallelize_hyperparameter_tuning.py
+++ ch6/parallelize_hyperparameter_tuning.py
@@ -1,23 +1,24 @@
 
 from ch4.ml_model import MlModel
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
 import pandas as pd
 import time
 
 parameters = {"n_estimators": (50, 100, 150, 200),
               "max_depth": (1, 2),
-              "learning_rate": (0.01, 0.05, 0.1),
-              "subsample": (0.5, 0.6, 0.7, 0.8)}
+              "min_samples_split": (100, 250, 500, 750, 1000)
+                 }
 
-gbm = MlModel(ml_model = GradientBoostingRegressor(),
+forest = MlModel(ml_model = RandomForestRegressor(),
                  parameters = parameters,
-                 n_jobs = 4,
-                 scoring = "roc_auc",
-                 n_iter = 30,
+                 n_jobs = 3,
+                 scoring = "neg_mean_squared_error",
+                 n_iter = 5,
                  random_state = 0)
 
 
 artists = pd.read_csv("data/artists.csv")
+
 
 artists = artists.dropna().reset_index(drop = True)
 
@@ -26,6 +27,6 @@ features = ["followers", "num_genres"]
 
 
 start = time.time()
-gbm.tune(artists[features], artists.popularity)
+forest.tune(artists[features], artists.popularity)
 end = time.time()
 print("Completed in ", end - start, " seconds")
